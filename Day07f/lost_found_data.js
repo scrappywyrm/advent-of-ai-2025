464f5fc5fca0d71f9274315e64512cc3ac020e9a
scrappywyrm <bgashirabake@gmail.com>
2025-12-26 04:13:34 -0800

    Day 7f: Completed Full Stack Solution (JSON Backend + HTML Dashboard)

---
+++ Day07f/lost_found_data.js
@@ -0,0 +1,38 @@
+// lost_found_data.js
+window.lostData = [
+  {
+    "item": "blue scarf",
+    "category": "Clothing",
+    "location": "ice skating area",
+    "time": "2:00 PM",
+    "urgent": false
+  },
+  {
+    "item": "iPhone 13 pro black case",
+    "category": "Electronics",
+    "location": "storytelling tent",
+    "time": "3:00 PM",
+    "urgent": true
+  },
+  {
+    "item": "keys toyota key fob + house keys",
+    "category": "Personal",
+    "location": "parking lot",
+    "time": "Unknown",
+    "urgent": true
+  },
+  {
+    "item": "wallet brown leather mens",
+    "category": "Personal",
+    "location": "fortune teller tent",
+    "time": "Unknown",
+    "urgent": true
+  },
+  {
+    "item": "red mitten kid size",
+    "category": "Clothing",
+    "location": "cocoa booth",
+    "time": "2:30 PM",
+    "urgent": false
+  }
+];
